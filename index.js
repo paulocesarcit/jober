c536b41747d2baffa073ca43c35d714f8d4898b1
paulocesarcit <paulocesarcit@hotmail.com>
2019-04-07 18:57:09 -0300

    server and security

--- index.js
+++ index.js
@@ -2,11 +2,23 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 
+const path = require('path')
 const sqlite = require('sqlite')
-const dbConnection = sqlite.open('banco.sqlite', { Promise })
+const dbConnection = sqlite.open(path.resolve(__dirname, 'banco.sqlite'), { Promise })
 
+const port = process.env.PORT || 3000
+
+app.use('/admin', (req, res, next) =>{
+    if (req.hostname === 'localhost'){
+        next()
+    } else {
+        res.send('Not Allowed!')
+    }
+})
+
+app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-app.use(express.static('public'))
+app.use(express.static(path.resolve(__dirname,'public')))
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', async(req, res) => {
@@ -90,7 +102,7 @@ const init = async() => {
 }
 init()
 
-app.listen(3000, (err) => {
+app.listen(port, (err) => {
     if (err) {
         console.log('Error on starting server')
     } else {
